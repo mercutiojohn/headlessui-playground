202a29b4b09248495aeaced7eaaf5b11d82a14c4
mercutio <mercutio_john@foxmail.com>
2023-08-30 22:36:31 +0800

    [fw] add: uno presets

--- uno.config.js
+++ uno.config.js
@@ -1,6 +1,9 @@
 // uno.config.js
 import { defineConfig } from 'unocss'
+import presetUno from '@unocss/preset-uno'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetUno(),
+  ],
 })
