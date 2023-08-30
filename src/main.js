84beb51891f275effc164cddf2c71b6a3c051e41
mercutio <mercutio_john@foxmail.com>
2023-08-30 22:33:40 +0800

    add: unocss

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
+// Styles
 import './assets/main.css'
+import 'virtual:uno.css'
 
+// Vue Official Components
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// --------------
 import App from './App.vue'
+
+// Env Components
 import router from './router'
 
+// --------------
 const app = createApp(App)
 
+// Env Components
 app.use(createPinia())
 app.use(router)
 
+// --------------
 app.mount('#app')
